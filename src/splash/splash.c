2c5f30192d40b5f9636e1bb9d241811149639442
MestreLion <linux@rodrigosilva.com>
2020-09-01 03:25:25 -0300

    splash: read the image data into a buffer

--- src/splash/splash.c
+++ src/splash/splash.c
@@ -1,31 +1,41 @@
 #include <assert.h>
+#include <errno.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 
-static const char * const PICFILE = "../rogue.pic";
+#define BSAVE_HEADER    7  // BSAVE header is 7 bytes in rogue.pic
 
-char* PROGNAME;  // == argv[0], set by main()
+
+static const char * const PICFILE = "../rogue.pic";
+static const char * PROGNAME = NULL;  // == argv[0], set by main()
+
 
 void usage(FILE* stream)
 {
 	fprintf(stream, "Usage: %s [-h|--help] [PICFILE]\n", PROGNAME);
 }
 
+
+// explain_output_error_and_die() using stdlib only
 _Noreturn void fatal(const char *fmt, ...)
 {
-	char msg[200];
+	char msg[1000];
 
 	va_list argp;
 	va_start(argp, fmt);
 	vsnprintf(msg, sizeof(msg), fmt, argp);
 	va_end(argp);
 
-	fprintf(stderr, "%s: %s\n", PROGNAME, msg);
+	if (errno)
+		// just like perror()!
+		fprintf(stderr, "%s: %s: %s\n", PROGNAME, strerror(errno), msg);
+	else
+		fprintf(stderr, "%s: %s\n", PROGNAME, msg);
 	usage(stderr);
-	exit(1);
+	exit(EXIT_FAILURE);
 }
 
 
@@ -33,6 +43,12 @@ int main(int argc, char* argv[])
 {
 	char* arg;
 	const char* path = NULL;
+	FILE* file = NULL;
+	/*
+	 * 0x4000 = 16384 bytes, the file size minus 7-byte header
+	 * Oh, the wonders of modern platforms and megabytes of stack space!
+	 */
+	unsigned char data[0x4000];
 
 	PROGNAME = argv[0];
 
@@ -66,9 +82,17 @@ int main(int argc, char* argv[])
 	if (argc)
 		fatal("too many arguments: %s", *(++argv));
 
-	assert(!argc);
+	assert(!argc && path);
 
-	printf("PIC path: %s\n", path);
+	if ((file = fopen(path, "rb")) == NULL)
+		fatal("%s", path);  // rely on strerror(errno) built-in message
 
-	return 0;
+	// Read the file data, discarding the header
+	if (!fread(data, BSAVE_HEADER, 1, file) ||
+	    !fread(data, sizeof(data), 1, file))
+	{
+		fclose(file);
+		fatal("error reading file: %s", path);  // no errno set for EOF :(
+	}
+	fclose(file);
 }
